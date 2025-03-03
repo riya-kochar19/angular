5917d302ab8b836487c3592fbb4618ff46728ab0
riya-kochar19 <riya.kochar@bellblazetech.com>
2025-03-03 15:15:56 +0530

    Update backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "statefilebackup99" # Replace with your bucket name
+    bucket         = "angulardemo-project" # Replace with your bucket name
     key            = "terraform.tfstate"
     region         = "us-east-1"
   }
