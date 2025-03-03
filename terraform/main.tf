5ee3a74c4ff49b6f81a7ce5dfa13bf84eb39f30b
riya-kochar19 <riya.kochar@bellblazetech.com>
2025-03-03 15:44:37 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,7 +5,7 @@ provider "aws" {
 resource "aws_instance" "calculatorAngular" {
   ami           = "ami-04b4f1a9cf54c11d0" # Your existing Ubuntu AMI
   instance_type = "t2.micro"
-  key_name      = "angular" # Your actual AWS key pair
+  key_name      = "angluar" # Your actual AWS key pair
 
   # Attach existing security group
   vpc_security_group_ids = ["sg-032789747ef521238"] # Your actual Security Group ID
