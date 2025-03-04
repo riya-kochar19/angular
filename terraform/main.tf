5bef31604cb22f2d823bd7841d94ead1f6138307
riya-kochar19 <riya.kochar@bellblazetech.com>
2025-03-04 11:26:42 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -19,8 +19,4 @@ resource "aws_eip" "angular_eip" {
   instance = aws_instance.calculatorAngular.id
 }
 
-# Output the static Elastic IP
-output "instance_public_ip" {
-  value = aws_eip.angular_eip.public_ip
-}
 
