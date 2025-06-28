e059e3bff800a66bec45cf5b7f97e4c06128c459
riya-kochar19 <riya.kochar@bellblazetech.com>
2025-06-28 20:13:46 +0530

    updated bucket name

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "angulardemo-project" # Replace with your bucket name
+    bucket         = "calci-fargate-bucket" # Replace with your bucket name
     key            = "terraform.tfstate"
     region         = "ap-northeast-3" # Replace with your AWS region
   }
