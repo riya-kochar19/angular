e9e1ff430a7f1c3b4a46394118fccd3939a7218b
riya-kochar19 <riya.kochar@bellblazetech.com>
2025-06-28 22:00:32 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -56,7 +56,7 @@ resource "aws_ecs_task_definition" "app" {
   container_definitions = jsonencode([
     {
       name      = "calculator-app"
-      image     = "${aws_ecr_repository.my_app.repository_url}:latest"
+      image     = "${aws_ecr_repository.my_app.repository_url}:${var.image_tag}"
       portMappings = [
         {
           containerPort = 80
