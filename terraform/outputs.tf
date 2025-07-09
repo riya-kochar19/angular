46380572d45055394273c1f579fb0173c134157b
riya-kochar19 <riya.kochar@bellblazetech.com>
2025-07-09 09:40:55 +0530

    updating output.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -5,4 +5,15 @@ output "ecr_repository_url" {
 output "ecs_cluster_name" {
   value = aws_ecs_cluster.main.name
 }
+output "green_tg_arn" {
+  value = aws_lb_target_group.green.arn
+}
+
+output "blue_tg_arn" {
+  value = aws_lb_target_group.blue.arn
+}
+
+output "alb_listener_arn" {
+  value = aws_lb_listener.http.arn
+}
 
