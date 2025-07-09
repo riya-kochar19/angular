0331e4fe994cf26056b5d5d3f24f3c40feb275d8
riya-kochar19 <riya.kochar@bellblazetech.com>
2025-07-09 11:25:21 +0530

    updated green listner

--- terraform/main.tf
+++ terraform/main.tf
@@ -63,6 +63,22 @@ resource "aws_lb_listener" "http" {
     target_group_arn = aws_lb_target_group.blue.arn
   }
 }
+resource "aws_lb_listener_rule" "green" {
+  listener_arn = aws_lb_listener.http.arn
+  priority     = 100
+
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.green.arn
+  }
+
+  condition {
+    path_pattern {
+      values = ["/green"]
+    }
+  }
+}
+
 
 
 resource "aws_ecs_task_definition" "app" {
