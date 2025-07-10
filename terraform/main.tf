4dcc015c834f8fbfb001fdabd66efceec010d99d
riya-kochar19 <riya.kochar@bellblazetech.com>
2025-07-10 19:55:48 +0530

    attached monitoring and logs

--- terraform/main.tf
+++ terraform/main.tf
@@ -50,6 +50,7 @@ resource "aws_cloudwatch_log_group" "ecs_logs" {
   retention_in_days =7
 }
 
+
 resource "aws_ecs_task_definition" "app" {
   family                   = "calculator-fargate-task"
   network_mode             = "awsvpc"
