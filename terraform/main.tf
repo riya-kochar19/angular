35a6ab505e2bad9c06d1c01de6adf0320c59d597
riya-kochar19 <riya.kochar@bellblazetech.com>
2025-07-11 14:01:46 +0530

    code deploy test

--- terraform/main.tf
+++ terraform/main.tf
@@ -104,7 +104,7 @@ resource "aws_ecs_task_definition" "app" {
 resource "aws_ecs_service" "app" {
   name            = "calculator-app-service"
   cluster         = aws_ecs_cluster.main.id
-
+  # task_definition = aws_ecs_task_definition.app.arn
   launch_type     = "FARGATE"
   desired_count   = 1
 
@@ -124,6 +124,10 @@ resource "aws_ecs_service" "app" {
     container_name   = "calculator-app"
     container_port   = 80
   }
+  lifecycle {
+    ignore_changes = [task_definition]
+  }
+
 
   depends_on = [aws_lb_listener.http]
 }
@@ -156,8 +160,33 @@ resource "aws_codedeploy_deployment_group" "ecs_group" {
   app_name              = aws_codedeploy_app.ecs_app.name
   deployment_group_name = "calculator-deploy-group"
   service_role_arn      = aws_iam_role.codedeploy_role.arn
-
   deployment_config_name = "CodeDeployDefault.ECSAllAtOnce"
+
+  deployment_style {
+    deployment_type = "BLUE_GREEN"
+    deployment_option = "WITH_TRAFFIC_CONTROL"
+  }
+
+  blue_green_deployment_config {
+    terminate_blue_instances_on_deployment_success {
+      action = "TERMINATE"
+      termination_wait_time_in_minutes = 5
+    }
+
+    deployment_ready_option {
+      action_on_timeout = "CONTINUE_DEPLOYMENT"
+      wait_time_in_minutes = 0
+    }
+
+    # green_fleet_provisioning_option {
+    #   action = "DISCOVER_EXISTING"
+    # }
+  }
+
+  auto_rollback_configuration {
+    enabled = true
+    events  = ["DEPLOYMENT_FAILURE"]
+  }
 
   ecs_service {
     cluster_name = aws_ecs_cluster.main.name
@@ -166,25 +195,17 @@ resource "aws_codedeploy_deployment_group" "ecs_group" {
 
   load_balancer_info {
     target_group_pair_info {
-      target_group {
-        name = aws_lb_target_group.blue.name
-      }
-      target_group {
-        name = aws_lb_target_group.green.name
-      }
       prod_traffic_route {
         listener_arns = [aws_lb_listener.http.arn]
       }
+
+      target_group {
+        name = aws_lb_target_group.blue.name
+      }
+
+      target_group {
+        name = aws_lb_target_group.green.name
+      }
     }
   }
-
-  auto_rollback_configuration {
-    enabled = true
-    events  = ["DEPLOYMENT_FAILURE"]
-  }
-
-  depends_on = [
-    aws_lb_listener.http,
-    aws_iam_role_policy_attachment.codedeploy_attachment
-  ]
 }
