46b535427a6d5f2f7faae06559a2e43e3d6cf15a
riya-kochar19 <riya.kochar@bellblazetech.com>
2025-07-11 12:09:41 +0530

    updated policy

--- terraform/main.tf
+++ terraform/main.tf
@@ -149,7 +149,7 @@ resource "aws_iam_role" "codedeploy_role" {
 
 resource "aws_iam_role_policy_attachment" "codedeploy_attachment" {
   role       = aws_iam_role.codedeploy_role.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSCodeDeployRoleForECS"
+  policy_arn = "arn:aws:iam::aws:policy/AWSCodeDeployRoleForECS"
 }
 
 resource "aws_codedeploy_deployment_group" "ecs_group" {
