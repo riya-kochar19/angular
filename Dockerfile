5edd7f5f37e742aa86ca359b03aad6ea2d517c61
riya-kochar19 <riya.kochar@bellblazetech.com>
2025-07-11 11:32:06 +0530

    codedeploy-deployment

--- Dockerfile
+++ Dockerfile
@@ -1,37 +1,13 @@
-# # Step 1: Build Angular App
-# FROM node:22 AS build-stage
-# WORKDIR /app
-# COPY package*.json ./
-# RUN npm install
-# COPY . .
-# RUN npm run build
-# # Step 2: Serve with Nginx
-# FROM nginx:alpine
-# COPY --from=build-stage /app/dist/calculator-app /usr/share/nginx/html
-# COPY nginx.conf /etc/nginx/conf.d/default.conf
-# EXPOSE 80
-# CMD ["nginx", "-g", "daemon off;"]
-# Step 1: Build Angular App (skip or simulate broken build)
+# Step 1: Build Angular App
 FROM node:22 AS build-stage
 WORKDIR /app
 COPY package*.json ./
 RUN npm install
 COPY . .
-
-# Optional: intentionally skip build or break it
-RUN echo "<h1>Broken build</h1>" > /app/dist/index.html
-
-# Step 2: Serve with broken Nginx
+RUN npm run build
+# Step 2: Serve with Nginx
 FROM nginx:alpine
-
-# DO NOT copy Angular build (simulate broken deploy)
-# COPY --from=build-stage /app/dist/calculator-app /usr/share/nginx/html
-
-# Or copy empty index to still run but fail health check
-COPY --from=build-stage /app/dist/index.html /usr/share/nginx/html/index.html
-
-# Break nginx: remove default config so it returns 403
-RUN rm /etc/nginx/conf.d/default.conf
-
+COPY --from=build-stage /app/dist/calculator-app /usr/share/nginx/html
+COPY nginx.conf /etc/nginx/conf.d/default.conf
 EXPOSE 80
 CMD ["nginx", "-g", "daemon off;"]
--- terraform/main.tf
+++ terraform/main.tf
@@ -26,14 +26,13 @@ resource "aws_lb_target_group" "blue" {
   target_type = "ip"
 
   health_check {
-  path                = "/missing.html"
-  matcher             = "200"
-  interval            = 30
-  timeout             = 5
-  healthy_threshold   = 2
-  unhealthy_threshold = 2
-}
-
+    path                = "/"
+    interval            = 30
+    timeout             = 5
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    matcher             = "200"
+  }
 }
 
 resource "aws_lb_target_group" "green" {
@@ -43,17 +42,18 @@ resource "aws_lb_target_group" "green" {
   vpc_id      = var.vpc_id
   target_type = "ip"
 
-   health_check {
-  path                = "/missing.html"
-  matcher             = "200"
-  interval            = 30
-  timeout             = 5
-  healthy_threshold   = 2
-  unhealthy_threshold = 2
-}
+  health_check {
+    path                = "/"
+    interval            = 30
+    timeout             = 5
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    matcher             = "200"
+  }
 }
 
-# Listener initially points to blue (GitHub Actions will switch later)
+
+
 resource "aws_lb_listener" "http" {
   load_balancer_arn = aws_lb.app_alb.arn
   port              = 80
@@ -64,64 +64,54 @@ resource "aws_lb_listener" "http" {
     target_group_arn = aws_lb_target_group.blue.arn
   }
 }
-resource "aws_lb_listener_rule" "green" {
-  listener_arn = aws_lb_listener.http.arn
-  priority     = 100
 
-  action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.green.arn
-  }
+resource "aws_cloudwatch_log_group" "ecs_logs" {
+  name              = "/ecs/calculator-app"
+  retention_in_days =7
+}
 
-  condition {
-    path_pattern {
-      values = ["/green"]
+
+resource "aws_ecs_task_definition" "app" {
+  family                   = "calculator-fargate-task"
+  network_mode             = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+  cpu                      = "256"
+  memory                   = "512"
+  execution_role_arn       = var.ecs_task_exec_role_arn
+
+  container_definitions = jsonencode([
+    {
+      name      = "calculator-app"
+      image     = "${aws_ecr_repository.my_app.repository_url}:${var.image_tag}"
+      portMappings = [
+        {
+          containerPort = 80
+          protocol       = "tcp"
+        }
+      ]
+      logConfiguration = {
+    logDriver = "awslogs"
+    options = {
+      awslogs-group         = "/ecs/calculator-app"
+      awslogs-region        = var.region
+      awslogs-stream-prefix = "ecs"
     }
   }
+    }
+  ])
 }
 
-resource "aws_ecs_task_definition" "green" {
-  family                   = "calculator-green-task"
-  network_mode             = "awsvpc"
-  requires_compatibilities = ["FARGATE"]
-  cpu                      = "256"
-  memory                   = "512"
-  execution_role_arn       = var.ecs_task_exec_role_arn
-
-  container_definitions = jsonencode([{
-    name      = "calculator-app"
-    image     = "${aws_ecr_repository.my_app.repository_url}:${var.image_tag}" # ← dynamic tag
-    portMappings = [{
-      containerPort = 80
-      protocol      = "tcp"
-    }]
-  }])
-}
-
-resource "aws_ecs_task_definition" "blue" {
-  family                   = "calculator-blue-task"
-  network_mode             = "awsvpc"
-  requires_compatibilities = ["FARGATE"]
-  cpu                      = "256"
-  memory                   = "512"
-  execution_role_arn       = var.ecs_task_exec_role_arn
-
-  container_definitions = jsonencode([{
-    name      = "calculator-app"
-    image     = "${aws_ecr_repository.my_app.repository_url}:stable" 
-    portMappings = [{
-      containerPort = 80
-      protocol      = "tcp"
-    }]
-  }])
-}
-
-resource "aws_ecs_service" "blue" {
-  name            = "calculator-blue"
+resource "aws_ecs_service" "app" {
+  name            = "calculator-app-service"
   cluster         = aws_ecs_cluster.main.id
-  task_definition = aws_ecs_task_definition.blue.arn
+  task_definition = aws_ecs_task_definition.app.arn
   launch_type     = "FARGATE"
   desired_count   = 1
+
+  
+  deployment_controller {
+    type = "CODE_DEPLOY"
+  }
 
   network_configuration {
     subnets          = var.public_subnets
@@ -138,24 +128,63 @@ resource "aws_ecs_service" "blue" {
   depends_on = [aws_lb_listener.http]
 }
 
-resource "aws_ecs_service" "green" {
-  name            = "calculator-green"
-  cluster         = aws_ecs_cluster.main.id
-  task_definition = aws_ecs_task_definition.green.arn
-  launch_type     = "FARGATE"
-  desired_count   = 1
+resource "aws_codedeploy_app" "ecs_app" {
+  name             = "calculator-codedeploy-app"
+  compute_platform = "ECS"
+}
+resource "aws_iam_role" "codedeploy_role" {
+  name = "codedeploy-role"
 
-  network_configuration {
-    subnets          = var.public_subnets
-    security_groups  = [var.ecs_sg]
-    assign_public_ip = true
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Effect = "Allow",
+      Principal = {
+        Service = "codedeploy.amazonaws.com"
+      },
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "codedeploy_attachment" {
+  role       = aws_iam_role.codedeploy_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSCodeDeployRoleForECS"
+}
+
+resource "aws_codedeploy_deployment_group" "ecs_group" {
+  app_name              = aws_codedeploy_app.ecs_app.name
+  deployment_group_name = "calculator-deploy-group"
+  service_role_arn      = aws_iam_role.codedeploy_role.arn
+
+  deployment_config_name = "CodeDeployDefault.ECSAllAtOnce"
+
+  ecs_service {
+    cluster_name = aws_ecs_cluster.main.name
+    service_name = aws_ecs_service.app.name
   }
 
-  load_balancer {
-    target_group_arn = aws_lb_target_group.green.arn
-    container_name   = "calculator-app"
-    container_port   = 80
+  load_balancer_info {
+    target_group_pair_info {
+      target_group {
+        name = aws_lb_target_group.blue.name
+      }
+      target_group {
+        name = aws_lb_target_group.green.name
+      }
+      prod_traffic_route {
+        listener_arns = [aws_lb_listener.http.arn]
+      }
+    }
   }
 
-  depends_on = [aws_lb_listener.http]
+  auto_rollback_configuration {
+    enabled = true
+    events  = ["DEPLOYMENT_FAILURE"]
+  }
+
+  depends_on = [
+    aws_lb_listener.http,
+    aws_iam_role_policy_attachment.codedeploy_attachment
+  ]
 }
